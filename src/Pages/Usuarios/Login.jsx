cc57d3551a1e8de646dedf8adb1410c1e01d54f3
Madiath <juanpipapa2002@gmail.com>
2026-04-27 15:01:50 -0300

    Funcionando pero deshabilitado el MFA

--- src/Pages/Usuarios/Login.jsx
+++ src/Pages/Usuarios/Login.jsx
@@ -98,12 +98,15 @@ const Login = () => {
 
       localStorage.setItem("usuario", data.email)
       localStorage.setItem("rol", data.rol)
+      //Comentar el token una vez integrado el MFA
+      localStorage.setItem("token", data.token)
 
-      await  primerPasoMfa({ email: data.email })
+      //await  primerPasoMfa({ email: data.email })
 
-      navigate("/authsecure")      
+      //MFA DESHABILITADO PARA TESTEO
+      //navigate("/authsecure")      
 
-      //navigate("/pacientes")
+      navigate("/pacientes")
 
     } catch (error) {
 
