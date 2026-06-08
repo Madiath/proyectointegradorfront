1c9bfa9ad9a62ef68c5909540d9ae19f2ebdefde
Guille301 <guillermogau300@gmail.com>
2026-06-08 17:39:40 -0300

    correcciones de auditorias en evolucion y historia

--- src/Services/evolucionService.js
+++ src/Services/evolucionService.js
@@ -1,33 +1,25 @@
-import axios from 'axios'
+import axiosInstance from './axiosInstance'
 import API_BASE_URL from './config'
 
 const BASE_URL = `${API_BASE_URL}/api/Evolucion`
 
 export const getEvolucionesPorPaciente = async (idPaciente) => {
-    const response = await axios.get(`${BASE_URL}/paciente/${idPaciente}`)
+    const response = await axiosInstance.get(`${BASE_URL}/paciente/${idPaciente}`)
     return response.data
 }
 
 export const altaEvolucion = async (evolucion) => {
-    const response = await axios.post(BASE_URL, evolucion)
+    const response = await axiosInstance.post(BASE_URL, evolucion)
     return response.data
 }
 
 export const editarEvolucion = async (id, evolucion) => {
-    const response = await axios.put(`${BASE_URL}/${id}`, evolucion)
+    const response = await axiosInstance.put(`${BASE_URL}/${id}`, evolucion)
     return response.data
 }
 
 export const getEvolucionesPorFecha = async (idPaciente, fechaDesde, fechaHasta) => {
-  console.log("idPaciente:", idPaciente);
-console.log("fechaDesde:", fechaDesde);
-console.log("fechaHasta:", fechaHasta);
-
-console.log(
-  `${BASE_URL}/filtrEvol/${idPaciente}?fechaDesde=${fechaDesde}&fechaHasta=${fechaHasta}`
-);
-  
-    const response = await axios.get(`${BASE_URL}/filtrEvol/${idPaciente}?fechaDesde=${fechaDesde}&fechaHasta=${fechaHasta}`);
-  return response.data;
-};
+    const response = await axiosInstance.get(`${BASE_URL}/filtrEvol/${idPaciente}?fechaDesde=${fechaDesde}&fechaHasta=${fechaHasta}`)
+    return response.data
+}
 
--- src/Services/historialClinicoService.js
+++ src/Services/historialClinicoService.js
@@ -1,31 +1,26 @@
-import axios from 'axios'
+import axiosInstance from './axiosInstance'
 import API_BASE_URL from './config'
 
 const BASE_URL = `${API_BASE_URL}/api/HistorialClinico`
 
 export const getHistorialClinico = async (idPaciente) => {
-    const response = await axios.get(`${BASE_URL}/paciente/${idPaciente}`)
+    const response = await axiosInstance.get(`${BASE_URL}/paciente/${idPaciente}`)
     return response.data
 }
 
 export const altaHistorialClinico = async (historial) => {
-    const response = await axios.post(BASE_URL, historial)
+    const response = await axiosInstance.post(BASE_URL, historial)
     return response.data
 }
 
 export const editarHistorialClinico = async (idPaciente, historial) => {
-    const response = await axios.put(`${BASE_URL}/paciente/${idPaciente}`, historial)
+    const response = await axiosInstance.put(`${BASE_URL}/paciente/${idPaciente}`, historial)
     return response.data
 }
 
-
 export const generarPdfHistorialClinico = async (idPaciente) => {
-  const response = await axios.get(
-    `${BASE_URL}/paciente/${idPaciente}/pdf`,
-    {
-      responseType: "blob",
-    }
-  );
-
-  return response;
-};
+    const response = await axiosInstance.get(`${BASE_URL}/paciente/${idPaciente}/pdf`, {
+        responseType: 'blob',
+    })
+    return response
+}
